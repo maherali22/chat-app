a65bc3c07cbf3e0531af32707cd26d70e5a5e0c0
Maher <ashrafchola@Ashrafs-MacBook-Air.local>
2024-11-28 14:50:41 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,8 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
